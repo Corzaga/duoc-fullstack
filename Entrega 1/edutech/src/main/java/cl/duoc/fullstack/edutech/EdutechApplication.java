2dc9f2f9130b1f3dfb61db1fca679a3f9e5a80b1
Corzaga <m.corderoaguilera@gmail.com>
2025-05-02 16:22:42 -0400

    Se sube el proyecto SPRING

---
+++ Entrega 1/edutech/src/main/java/cl/duoc/fullstack/edutech/EdutechApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.fullstack.edutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechApplication.class, args);
+	}
+
+}
